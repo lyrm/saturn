1b5b0664804849d7281dfd77589d0b263f907930
Carine Morel <carine@tarides.com>
2024-10-24 17:00:00 +0200

    Update benchs.

--- bench/bench_htbl.ml
+++ bench/bench_htbl.ml
@@ -10,8 +10,6 @@ end
 let run_one ~budgetf ~n_domains ?(n_ops = 20 * Util.iter_factor)
     ?(n_keys = 10000) ~percent_mem ?(percent_add = (100 - percent_mem + 1) / 2)
     ?(prepopulate = true) ~unsafe (module Htbl : Htbl_intf.HTBL) =
-  let percent_rem = 100 - (percent_mem + percent_add) in
-
   let limit_mem = percent_mem in
   let limit_add = percent_mem + percent_add in
 
@@ -19,57 +17,53 @@ let run_one ~budgetf ~n_domains ?(n_ops = 20 * Util.iter_factor)
   assert (limit_mem <= limit_add && limit_add <= 100);
 
   let t = Htbl.create ~hashed_type:(module Key) () in
-  if prepopulate then
-    for _ = 1 to n_keys do
-      let value = Random.bits () in
-      let key = value mod n_keys in
-      Htbl.try_add t key value |> ignore
-    done;
 
   let n_ops = (100 + percent_mem) * n_ops / 100 in
   let n_ops = n_ops * n_domains in
 
-  let n_ops_todo = Atomic.make 0 |> Multicore_magic.copy_as_padded in
+  let n_ops_todo = Countdown.create ~n_domains () in
 
-  let init _ =
-    Atomic.set n_ops_todo n_ops;
-    Random.State.make_self_init ()
+  let before () =
+    let _ : _ Seq.t = Htbl.remove_all t in
+    Countdown.non_atomic_set n_ops_todo n_ops
   in
-  let work _ state =
+  let init i =
+    let state = Random.State.make_self_init () in
+    if prepopulate then begin
+      let n = ((i + 1) * n_keys / n_domains) - (i * n_keys / n_domains) in
+      for _ = 1 to n do
+        let value = Random.State.bits state in
+        let key = value mod n_keys in
+        Htbl.try_add t key value |> ignore
+      done
+    end;
+    state
+  in
+  let work domain_index state =
     let rec work () =
-      let n = Util.alloc n_ops_todo in
-      if n <> 0 then
-        let rec loop n =
-          if 0 < n then
-            let value = Random.State.bits state in
-            let op = (value asr 20) mod 100 in
-            let key = value mod n_keys in
-            if op < limit_mem then begin
-              Htbl.mem t key |> ignore;
-              loop (n - 1)
-            end
-            else if op < limit_add then begin
-              Htbl.try_add t key value |> ignore;
-              loop (n - 1)
-            end
-            else begin
-              Htbl.try_remove t key |> ignore;
-              loop (n - 1)
-            end
-          else work ()
-        in
-        loop n
+      let n = Countdown.alloc n_ops_todo ~domain_index ~batch:1000 in
+      if n <> 0 then begin
+        for _ = 1 to n do
+          let value = Random.State.bits state in
+          let op = (value asr 20) mod 100 in
+          let key = value mod n_keys in
+          if op < percent_mem then
+            match Htbl.find_exn t key with _ -> () | exception Not_found -> ()
+          else if op < limit_add then Htbl.try_add t key value |> ignore
+          else Htbl.try_remove t key |> ignore
+        done;
+        work ()
+      end
     in
     work ()
   in
-
   let config =
-    Printf.sprintf "%d workers, %d%% mem %d%% add %d%% rem %s" n_domains
-      percent_mem percent_add percent_rem
+    Printf.sprintf "%d worker%s, %d%% reads %s" n_domains
+      (if n_domains = 1 then "" else "s")
+      percent_mem
       (if unsafe then " (unsafe)" else "")
   in
-
-  Times.record ~budgetf ~n_domains ~init ~work ()
+  Times.record ~budgetf ~n_domains ~before ~init ~work ()
   |> Times.to_thruput_metrics ~n:n_ops ~singular:"operation" ~config
 
 let run_suite ~budgetf =
